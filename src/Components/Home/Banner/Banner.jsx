efd67446a2d86100f3a0ff0b3e9e50f602b510c4
FazleRifat <fazlerifatghost21@gmail.com>
2023-09-22 21:19:47 +0600

    banner pic

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -3,8 +3,8 @@ const Banner = () => {
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <img
-          src="/images/stock/photo-1635805737707-575885ab0820.jpg"
-          className="max-w-sm rounded-lg shadow-2xl"
+          src={'https://i.ibb.co/YhmbLp4/user.png'}
+          className="max-w-sm rounded-lg "
         />
         <div>
           <h1 className="text-5xl font-bold">
