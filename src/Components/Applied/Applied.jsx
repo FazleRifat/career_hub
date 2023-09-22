c0b6554b9ef28ac44ad4df16b1002d766b0611d5
FazleRifat <fazlerifatghost21@gmail.com>
2023-09-22 10:47:52 +0600

    first commit

---
+++ src/Components/Applied/Applied.jsx
@@ -0,0 +1,9 @@
+const Applied = () => {
+    return (
+        <div>
+            <h1>Applied</h1>
+        </div>
+    );
+};
+
+export default Applied;
---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Root from './Components/Root/Root';
+import Home from './Components/Home/Home';
+import Statistics from './Components/Statistics/Statistics';
+import Blog from './Components/Blog/Blog';
+import Applied from './Components/Applied/Applied';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>
+      },
+      {
+        path:'/static',
+        element:<Statistics></Statistics>
+      },
+      {
+        path:'/applied',
+        element:<Applied></Applied>
+      },
+      {
+        path:'/blog',
+        element:<Blog></Blog>
+      }
+    ]
+  },
+]);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
